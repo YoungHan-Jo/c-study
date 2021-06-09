49906697f2b814a4e13914ec15142c5c43e41965
YoungHan-Jo <81964439+YoungHan-Jo@users.noreply.github.com>
2021-06-09 21:44:47 +0900

    0609

--- codeUp/codeUp/codeUp.c
+++ codeUp/codeUp/codeUp.c
@@ -2,16 +2,21 @@
 #include <stdio.h>
 #include <math.h>
 
-
 int main()
 {
 	int n;
+	int count = 0;
 
 	scanf("%d", &n);
 
-	for (int i = 1; i <= 9; ++i) {
-		printf("%d*%d=%d\n", n, i, n * i);
+	for (int i = 1; i <= 1000000000; i *= 10) {
+		if (n / i == 0)
+			break;
+		else
+			count++;
 	}
+
+	printf("%d", count);
 
 	return 0;
 }
