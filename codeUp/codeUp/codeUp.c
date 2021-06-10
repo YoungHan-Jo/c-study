537b278334bb25975b29fdeec12c5150127998c2
YoungHan-Jo <81964439+YoungHan-Jo@users.noreply.github.com>
2021-06-10 20:16:29 +0900

    0610

--- codeUp/codeUp/codeUp.c
+++ codeUp/codeUp/codeUp.c
@@ -2,21 +2,9 @@
 #include <stdio.h>
 #include <math.h>
 
+
 int main()
 {
-	int n;
-	int count = 0;
-
-	scanf("%d", &n);
-
-	for (int i = 1; i <= 1000000000; i *= 10) {
-		if (n / i == 0)
-			break;
-		else
-			count++;
-	}
-
-	printf("%d", count);
 
 	return 0;
 }
