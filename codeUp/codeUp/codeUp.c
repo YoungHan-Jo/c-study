99193636272cc0b0133557a861f690523aa7a6a1
JYH <verin4494@gmail.com>
2021-06-16 08:55:15 +0900

    0616

--- codeUp/codeUp/codeUp.c
+++ codeUp/codeUp/codeUp.c
@@ -5,6 +5,24 @@
 
 int main()
 {
+	int n;
+
+	scanf("%d", &n);
+
+	for (int i = 1; i <= n; ++i) {
+		if (i == 1 || i == n) {
+			for (int j = 1; j <= n; ++j)
+				printf("*");
+		}
+		else {
+			printf("*");
+			for (int j = 1; j <= n - 2; ++j)
+				printf(" ");
+			printf("*");
+		}
+		printf("\n");
+	}
+
 
 	return 0;
 }
